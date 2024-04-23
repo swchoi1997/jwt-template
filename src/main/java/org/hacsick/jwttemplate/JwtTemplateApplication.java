b7975e12334fc84ed7699eb704ae6069dff96a35
SangWon <swchoi1997@naver.com>
2024-04-23 22:27:26 +0900

    Initial commit

---
+++ src/main/java/org/hacsick/jwttemplate/JwtTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.hacsick.jwttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTemplateApplication.class, args);
+    }
+
+}
